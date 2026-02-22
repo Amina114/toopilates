c7c58794e195bfd4889f26e9fccd2fee3c3439f1
zeinebchabchoub01-byte <zeineb.chabchoub.01@gmail.com>
2026-02-22 19:20:34 +0100

    font_coorection

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,23 @@
 import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { Inter } from "next/font/google";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="fr">
-      <body className="min-h-screen bg-white text-black">
+      <body className={inter.className}>
         <Header />
 
-        {/* ✅ full width, pas de max-width global */}
         <main>{children}</main>
 
         <Footer />
